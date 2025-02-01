a825c85752d8cb19b5f88371e8af20d1c40db9f2
Rishabh503 <rishabhtripathi2022@gmail.com>
2025-02-01 16:46:20 +0530

    second ui

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const NavBar = () => {
   return (
-    <div className='h-[52vw] w-screen bg-[#B0BEC5 ]'>
+    <div className='h-[54vw] w-screen bg-[#B0BEC5 ]'>
       <div className='h-[50vw] w-full -mt-2 p-4  border-black flex justify-between  '>
         <div className='w-3/5 h-full mt-4 p-3 content-center '>
         <p className='font-bold font-mono text-2xl'>
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -16,14 +16,14 @@ export const Result = () => {
   
 
   return (
-    <div className='w-screen h-auto flex flex-col  items-center'>
-     <p className='font-extrabold text-center sm:-mt-24 text-[#8B0000] text-3xl p-2 font-mono'>
+    <div className='w-screen h-auto flex flex-col m items-center'>
+     <p className='font-extrabold text-center sm:-mt-24 text-[#fa2b2b] text-4xl underline p-2 font-mono'>
         🏆 RESULTS 🏆
      </p>
         <div className='h-auto flex w-screen p-1 flex-col gap-4'>
           {
             Data.map((data)=>(
-              <div className='flex text-[#FFFFFF] text-lg font-semibold justify-between  px-8 p-4 rounded-2xl bg-[#757575]  '>
+              <div className='flex text-[#000000] text-lg font-semibold justify-between  px-8 p-4 rounded-2xl bg-[#bd4c13]  '>
                   <p>
                     {data.className}
                   </p>
