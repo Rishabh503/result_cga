0beddf2682ab16a3b384de742b89c1634e863ea4
Rishabh503 <rishabhtripathi2022@gmail.com>
2025-02-01 20:11:26 +0530

    added links and working part 2

---
+++ src/components/ResultDashBoard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const ResultDashBoard = ({value}) => {
+  return (
+    <div className='h-[50vw] w-full flex flex-col bg-cyan-300 border-xl px-8 py-12'>
+            <div>
+                RESULTS WILL BE LIVE SOON SAY TUNED
+            </div>
+
+            <div>
+                {value}
+            </div>
+
+    </div>
+  )
+}
